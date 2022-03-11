51e9126d124aede82956b1cbc9d5efc97c580666
AlexisX01 <101349878+AlexisX01@users.noreply.github.com>
2022-03-10 21:01:22 -0500

    Create 03_personal-info.py

---
+++ 03_personal-info.py
@@ -0,0 +1,11 @@
+print("Ingrese sus datos para la entrega del producto ") 
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido: ")
+ubic=input("Ingrese su ubicación: ")
+edad=input("Ingrese su edad: ")
+espacio=" "
+
+print("\n")
+print("La entrega se realizará a nombre de "+nombre+espacio+apellido+espacio+
+      "de"+ espacio + edad + espacio + "años de edad"+ espacio+
+      "a la dirección de "+ubic)
